29e707822f6036277e2fb8a8f2d604bde8b308cf
Andrew Hale <andyhalebmx@hotmail.com>
2019-10-23 11:05:53 +0100

    todo list is now working

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import Home from './pages/index';
 import Counter from './pages/counter';
-import Todo from './pages/to-do-list';
+import Todos from './pages/to-do-list';
 import Nav from './components/Nav';
 import './styles/main.scss';
 
@@ -16,7 +16,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/counter" component={Counter} />
-      <Route path="/to-do-list" component={Todo} />
+      <Route path="/to-do-list" component={Todos} />
     </Switch>
   </Router>
 )
--- src/pages/to-do-list.js
+++ src/pages/to-do-list.js
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import uuid4 from "uuid4";
 
-const Todo = () => (
-    <div className="home-container">
-        <h1>To-Do List</h1>
+function ToDos() {
+    const [todos, setTodos] = useState([
+        {
+            id: uuid4(),
+            todos: 'One'
+        },
+        {
+            id: uuid4(),
+            todos: 'Two'
+        },
+        {
+            id: uuid4(),
+            todos: 'Three'
+        }
+    ])
+
+    const [todoInput, settodoInput] = useState('');
+
+    function handleSubmit(e) {
+        // Stop form from submitting
+        e.preventDefault();
+        setTodos([
+          ...todos, {
+            id: uuid4(),
+            todos: todoInput
+          }
+        ])
+        settodoInput('');
+      }
+
+      function handleChange(e) {
+        settodoInput(e.target.value);
+        console.log(e.target.value);
+      }
+
+      function handleRemove(id) {
+        setTodos(todos.filter((todos) => todos.id !== id));
+      }
+
+      const test = uuid4();
+      console.log(test);
+
+    return (
+    <div className="todolist">
+      <h1>To-Do List</h1>
+      <h3>Things to do</h3>
+      <p className="explanation">Click on an item to remove it</p>
+      { todos.map(({ id, todos }) => (
+        <p onClick={() => handleRemove(id)} key={id}>{todos}</p>
+      )) }
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="">
+            Add To-Do:
+            <input type="text" onChange={handleChange} value={todoInput} />
+          </label>
+          <button>Add</button>
+        </form>
+      </div>
     </div>
-)
+    );
+}
 
-export default Todo;
+export default ToDos;
